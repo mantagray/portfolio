b0cdd1e23efe4ab401bb8d9ff115be7940a8daae
mantagray <132023019+mantagray@users.noreply.github.com>
2023-04-27 19:04:23 -0600

    First commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const hamburguerButton = document.getElementById('hamburguer')
+const navList = document.getElementById('nav-list')
+
+
+function toggleButton() {
+    navList.classList.toggle('show')
+}
+
+hamburguerButton.addEventListener('click', toggleButton)
